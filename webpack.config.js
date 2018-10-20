7668913fdf9187c33206388606eda0718ce8fde7
Osman Ali <okali@uci.edu>
2018-10-20 16:48:43 -0700

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'none',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                   'style-loader',
+                   'css-loader'
+                ]
+            }
+        ]
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    }
+};
